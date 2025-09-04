11e626c52cbb2c7bd73bb9c1234981442177fef7
hoanguser <137585402+hoanguser@users.noreply.github.com>
2025-09-04 14:06:57 +0700

    update-route

--- src/routers/Index.jsx
+++ src/routers/Index.jsx
@@ -27,10 +27,7 @@ const routers = [
             },
             {
                 path: "/detail/:movieID",
-                element:
-                    <AuthCheck isNeedLogin={true}>
-                        <MovieDetailPage />
-                    </AuthCheck>
+                element: <MovieDetailPage />,
             },
             {
                 path: "/ticket/:showTimeID",
